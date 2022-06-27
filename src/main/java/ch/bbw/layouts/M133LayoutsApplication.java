9b1d6231a8571f744ebb6e6765b0ca40b98e3bf9
rb-bbw <remo.bergamin@lernende.bbw.ch>
2022-06-27 16:30:20 +0200

    Init

---
+++ src/main/java/ch/bbw/layouts/M133LayoutsApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.layouts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M133LayoutsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M133LayoutsApplication.class, args);
+    }
+
+}
